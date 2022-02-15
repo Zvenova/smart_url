7b9ca95ea6f0bdc11b0749ebcf638394e71877f2
Zvenova <wadozzz178@gmail.com>
2022-02-15 20:58:13 +0300

    psvm

---
+++ src/main/java/com/zvenova/like_my/LikeMy.java
@@ -0,0 +1,12 @@
+package com.zvenova.like_my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeMy {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeMy.class, args);
+    }
+}
