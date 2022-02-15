fc5a51fa282d804318677ceaf6bb84e8185eedce
Zvenova <wadozzz178@gmail.com>
2022-02-16 01:12:19 +0300

    for teacher cuz testssssssssssssssssssssss

--- src/main/java/com/zvenova/like_my/domain/entity/User.java
+++ src/main/java/com/zvenova/like_my/domain/entity/User.java
@@ -1,20 +1,21 @@
 package com.zvenova.like_my.domain.entity;
 
 import com.zvenova.like_my.domain.security.Role;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Set;
 
 
 @Data
 @Table(name = "usr")
 @Entity
-@Builder
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -28,4 +29,29 @@ public class User {
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive();
+    }
 }
---
+++ src/main/java/com/zvenova/like_my/service/UserService.java
@@ -0,0 +1,16 @@
+package com.zvenova.like_my.service;
+
+import com.zvenova.like_my.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+}
