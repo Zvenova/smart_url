3b99e612a8ea879ce5378a983de108691d2f448b
Zvenova <wadozzz178@gmail.com>
2022-02-23 15:34:43 +0300

    add configure for permit all

--- src/main/java/com/zvenova/like_my/config/WebSecurityConfig.java
+++ src/main/java/com/zvenova/like_my/config/WebSecurityConfig.java
@@ -3,9 +3,11 @@ package com.zvenova.like_my.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -40,5 +42,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(HttpMethod.GET)
+                .antMatchers(HttpMethod.POST)
+                .antMatchers("/**");
     }
 }
