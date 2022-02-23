7075eaa28b78099b3e98b46a44a50fa95d17620c
Zvenova <wadozzz178@gmail.com>
2022-02-23 15:35:38 +0300

    create usersApiController, create user, test

--- src/main/java/com/zvenova/like_my/api/users/UsersApiController.java
+++ src/main/java/com/zvenova/like_my/api/users/UsersApiController.java
@@ -1,49 +1,34 @@
 package com.zvenova.like_my.api.users;
 
-import java.util.Set;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.zvenova.like_my.service.mapper.UserMapper;
+import org.springframework.web.bind.annotation.*;
 
 import com.zvenova.like_my.api.users.request.CreateUserRequest;
 import com.zvenova.like_my.api.users.response.CreateUserResponse;
 import com.zvenova.like_my.domain.entity.User;
-import com.zvenova.like_my.domain.security.Role;
-import com.zvenova.like_my.exception.UserIsAlreadyPresentException;
+import com.zvenova.like_my.exception.user.UserIsAlreadyPresentException;
 import com.zvenova.like_my.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController("/api/user")
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UsersApiController {
 
     private final UserService userService;
 
+    private final UserMapper userMapper;
+
     @PostMapping
-    public CreateUserResponse createUser(CreateUserRequest request)
-            throws UserIsAlreadyPresentException {
+    public CreateUserResponse createUser(
+            @RequestBody CreateUserRequest request
+    ) throws UserIsAlreadyPresentException {
 
-        User createdUser = userService.saveUser(getUserFromCreateUserRequest(request));
-        return getCreateUserResponse(createdUser);
-
+        User createdUser = userService.saveUser(userMapper.getUserFromCreateUserRequest(request));
+        return userMapper.getCreateUserResponse(createdUser);
     }
-
-    // TODO to mapper service
-    private CreateUserResponse getCreateUserResponse(User createdUser) {
-
-        return new CreateUserResponse(createdUser.getId(), createdUser.getUsername());
-    }
-
-    private User getUserFromCreateUserRequest(CreateUserRequest request) {
-
-        User userForCreation = new User();
-
-        userForCreation.setUsername(request.getUsername());
-        userForCreation.setPassword(request.getPassword());
-        userForCreation.setRoles(Set.of(Role.USER));
-        userForCreation.setActive(true);
-        return userForCreation;
-    }
-
 }
--- src/test/java/com/zvenova/like_my/api/users/UsersApiControllerTest.java
+++ src/test/java/com/zvenova/like_my/api/users/UsersApiControllerTest.java
@@ -1,8 +1,14 @@
 package com.zvenova.like_my.api.users;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.zvenova.like_my.domain.entity.User;
+import com.zvenova.like_my.domain.security.Role;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
@@ -10,17 +16,36 @@ import com.zvenova.like_my.base.BaseApiTest;
 
 import lombok.SneakyThrows;
 
+import java.util.Set;
+
+
 class UsersApiControllerTest extends BaseApiTest {
 
-    @SneakyThrows
-    @Test
-    public void testUserCreate() {
+    @Nested
+    public class TestCreateUser {
 
-        mockMvc.perform(post("/api/user").contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content("{\"name\": \"Cool Gadget\", \"description\": \"Looks cool\"}"))
-                .andExpect(status().isOk());
+        @Test
+        @SneakyThrows
+        public void testUserCreate_Valid() {
 
+            User testUser = getTestUser();
+            String content = new JSONObject()
+                    .put("username", testUser.getUsername())
+                    .put("password", testUser.getPassword())
+                    .toString();
+            doReturn(testUser).when(userRepository).save(testUser);
+
+            mockMvc.perform(post("/api/users").contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON)
+                            .content(content))
+                    .andExpect(status().isOk());
+            verify(userRepository, times(1)).save(testUser);
+        }
     }
 
+    private User getTestUser() {
+
+        return User.builder().username("Олег").active(true).password("123456")
+                .roles(Set.of(Role.USER)).build();
+    }
 }
