7b7ca28e9ead736cea3475f8f5d5d14900cf1c57
Zvenova <wadozzz178@gmail.com>
2022-03-15 22:04:10 +0300

    fix valid test for DbUserDetailsService.loadUserByUsernameIsPresent

--- src/test/java/com/zvenova/like_my/config/DBUserDetailsServiceTests.java
+++ src/test/java/com/zvenova/like_my/config/DBUserDetailsServiceTests.java
@@ -1,31 +1,66 @@
 package com.zvenova.like_my.config;
 
 import java.util.Collections;
+import java.util.Optional;
 
+import com.zvenova.like_my.exception.user.UserIsAlreadyPresentException;
+import com.zvenova.like_my.service.UserService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.zvenova.like_my.base.BaseTestWithDB;
 import com.zvenova.like_my.base.BaseTestWithoutDB;
 import com.zvenova.like_my.domain.entity.User;
 import com.zvenova.like_my.domain.security.Role;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class DBUserDetailsServiceTests extends BaseTestWithoutDB {
 
     @Autowired
     private DBUserDetailsService dbUserDetailsService;
 
-    @Test
-    // todo mock user service and write tests
-    public void loadUserByUsername_Valid() {
+    @MockBean
+    @Autowired
+    private UserService userService;
 
-        User insertedUser = userRepository.save(getTestUser());
+    @Nested
+    public class TestLoadUserByUsername {
 
-        UserDetails resultUser = dbUserDetailsService.loadUserByUsername(insertedUser.getUsername());
+        @Test
+        public void loadUserByUsernameIsPresent() throws UserIsAlreadyPresentException {
 
-        Assertions.assertThat(resultUser.getUsername()).isEqualTo(insertedUser.getUsername());
+            User testUser = getTestUser();
+
+            doReturn(testUser).when(userService).saveUser(testUser);
+            doReturn(testUser).when(userService).findByUsername(testUser.getUsername());
+
+            User insertedUser = userService.saveUser(testUser);
+
+            UserDetails resultUser = dbUserDetailsService.loadUserByUsername(insertedUser.getUsername());
+
+            Assertions.assertThat(resultUser.getUsername()).isEqualTo(insertedUser.getUsername());
+            verify(userService, times(1)).saveUser(testUser);
+            verify(userService, times(1)).findByUsername(testUser.getUsername());
+        }
+
+        @Test
+        public void loadUserByUsernameNotPresent() {
+
+            User testUser = getTestUser();
+
+            doThrow(new UsernameNotFoundException(testUser.getUsername()))
+                    .when(userService).findByUsername(testUser.getUsername());
+
+            assertThrows(UsernameNotFoundException.class,
+                    () -> dbUserDetailsService.loadUserByUsername(testUser.getUsername()));
+            verify(userService, times(1)).findByUsername(testUser.getUsername());
+        }
     }
 
     private User getTestUser() {
