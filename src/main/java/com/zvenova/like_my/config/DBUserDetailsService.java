67d6a2bdd0e67e723455c6a6fe119d1e4a846f77
Zvenova <wadozzz178@gmail.com>
2022-03-15 22:05:37 +0300

    rename userService.loadUserByUsername to findByUsername

--- src/main/java/com/zvenova/like_my/config/DBUserDetailsService.java
+++ src/main/java/com/zvenova/like_my/config/DBUserDetailsService.java
@@ -20,7 +20,7 @@ public class DBUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        return userService.loadUserByUsername(username);
+        return userService.findByUsername(username);
     }
 
 }
